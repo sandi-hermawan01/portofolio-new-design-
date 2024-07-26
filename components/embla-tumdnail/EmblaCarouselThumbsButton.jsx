a57a61a86f3fcf70fd3ab03eac537c14d8ecb504
sandi-hermawan01 <sandihermawanf01@gmail.com>
2024-07-26 21:05:55 +0700

    -

--- components/embla-tumdnail/EmblaCarouselThumbsButton.jsx
+++ components/embla-tumdnail/EmblaCarouselThumbsButton.jsx
@@ -16,7 +16,11 @@ export const Thumb = (props) => {
         type="button"
         className="embla-thumbs__slide__number  rounded-3xl bg-transparent touch-manipulation inline-flex no-underline cursor-pointer border-0 p-0 m-0 shadow-[inset_0_0_0_0.2rem_var(--detail-medium-contrast)] text-[1.8rem] font-semibold text-[var(--detail-high-contrast)] items-center justify-center h-[var(--thumbs-slide-height)] w-full [tap-highlight-color:rgba(var(--text-high-contrast-rgb-value),0.5)]"
       >
-        {index + 1}
+        <img
+          className="bg-cover"
+          src={`https://picsum.photos/600/350?v=${index}`}
+          alt="Your alt text"
+        />
       </button>
     </div>
   );
