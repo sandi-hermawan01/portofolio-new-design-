0c9fa49237d7e01158919d081e72edd0485ea985
sandi-hermawan01 <sandihermawanf01@gmail.com>
2024-07-27 21:58:23 +0700

    update carousel

--- components/embla-tumdnail/EmblaCarouselThumbsButton.jsx
+++ components/embla-tumdnail/EmblaCarouselThumbsButton.jsx
@@ -2,24 +2,62 @@
 
 import React from "react";
 
+const sliderData = [
+  {
+    id: 1,
+    image: "/assets/concept/img1.png",
+    title: "Slider 01",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 2,
+    image: "/assets/concept/img2.jpg",
+    title: "Slider 02",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 3,
+    image: "/assets/concept/img3.jpg",
+    title: "Slider 03",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 3,
+    image: "/assets/concept/img4.jpg",
+    title: "Slider 04",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 3,
+    image: "/assets/concept/img5.jpg",
+    title: "Slider 05",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+];
+
 export const Thumb = (props) => {
   const { selected, index, onClick } = props;
 
   return (
     <div
-      className={"embla-thumbs__slide flex-[0_0_22%] min-w-0 sm:flex-[0_0_15%] border-2 border-primary/40".concat(
-        selected ? " embla-thumbs__slide--selected" : ""
+      className={"embla-thumbs__slide flex-[0_0_0%] bg-cover min-w-0 sm:flex-[0_0_65%] border-2 border-primary/40".concat(
+        selected ? " embla-thumbs__slide--selected " : ""
       )}
     >
       <button
         onClick={onClick}
         type="button"
-        className="embla-thumbs__slide__number  rounded-3xl bg-transparent touch-manipulation inline-flex no-underline cursor-pointer border-0 p-0 m-0 shadow-[inset_0_0_0_0.2rem_var(--detail-medium-contrast)] text-[1.8rem] font-semibold text-[var(--detail-high-contrast)] items-center justify-center h-[var(--thumbs-slide-height)] w-full [tap-highlight-color:rgba(var(--text-high-contrast-rgb-value),0.5)]"
+        className="embla-thumbs__slide__number rounded-3xl bg-transparent touch-manipulation inline-flex no-underline cursor-pointer border-0 p-0 m-0 shadow-[inset_0_0_0_0.2rem_var(--detail-medium-contrast)] text-[1.8rem] font-semibold text-[var(--detail-high-contrast)] items-center justify-center h-[var(--thumbs-slide-height)] w-full [tap-highlight-color:rgba(var(--text-high-contrast-rgb-value),0.5)]"
       >
         <img
-          className="bg-cover"
-          src={`https://picsum.photos/600/350?v=${index}`}
-          alt="Your alt text"
+          className="h-[25vh] bg-cover bg-no-repeat bg-center object-cover"
+          src={sliderData[index].image}
+          alt={sliderData[index].title}
         />
       </button>
     </div>
--- components/embla-tumdnail/EmblaCarouselTumdnail.jsx
+++ components/embla-tumdnail/EmblaCarouselTumdnail.jsx
@@ -4,47 +4,46 @@ import React, { useState, useEffect, useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { Thumb } from "./EmblaCarouselThumbsButton";
 
+const sliderData = [
+  {
+    id: 1,
+    image: "/assets/concept/img1.png",
+    title: "Slider 01",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 2,
+    image: "/assets/concept/img2.jpg",
+    title: "Slider 02",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 3,
+    image: "/assets/concept/img3.jpg",
+    title: "Slider 03",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 3,
+    image: "/assets/concept/img4.jpg",
+    title: "Slider 04",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+  {
+    id: 3,
+    image: "/assets/concept/img5.jpg",
+    title: "Slider 05",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
+  },
+];
+
 const EmblaCarouselTumdnail = (props) => {
-  const {
-    slides = [
-      {
-        id: 1,
-        image: "/assets/concept/img1.png",
-        title: "Slider 01",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
-      },
-      {
-        id: 2,
-        image: "/assets/concept/img1.png",
-        title: "Slider 02",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
-      },
-      {
-        id: 3,
-        image: "/assets/concept/img1.png",
-        title: "Slider 03",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
-      },
-      {
-        id: 3,
-        image: "/assets/concept/img1.png",
-        title: "Slider 04",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
-      },
-      {
-        id: 3,
-        image: "/assets/concept/img1.png",
-        title: "Slider 05",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.",
-      },
-    ],
-    options,
-  } = props;
+  const { slides, options } = props;
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options);
   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
@@ -74,22 +73,22 @@ const EmblaCarouselTumdnail = (props) => {
   }, [emblaMainApi, onSelect]);
 
   return (
-    <div className="embla grid grid-cols-[70%_minmax(0,_1fr)] justify-center items-center w-full mt-[4rem] gap-5 px-5 mx-auto [--slide-height:38rem] [--slide-spacing:1rem] [--slide-size:100%]">
+    <div className="embla grid grid-cols-[70%_minmax(0,_1fr)] justify-center items-center w-full sm:mt-[5.5rem] mt-[2.5rem] gap-5 px-5 mx-auto [--slide-height:38rem] [--slide-spacing:1rem] [--slide-size:100%]">
       <div
         className="embla__viewport overflow-hidden border-2 border-primary/40"
         ref={emblaMainRef}
       >
         <div className="embla__container backface-hidden flex touch-pan-y -ml-[var(--slide-spacing)]">
-          {slides.map((index) => (
+          {sliderData.map((slides, index) => (
             <div
               className="embla__slide flex-[0_0_var(--slide-size)] min-w-0 pl-[var(--slide-spacing)]"
-              key={index}
+              key={slides.id}
             >
               <div className="embla__slide__number shadow-[inset_0_0_0_0.2rem_var(--detail-medium-contrast)] rounded-3xl text-4xl font-semibold flex items-center justify-center h-[var(--slide-height)] select-none">
                 <img
-                  className="embla__slide__img"
-                  src={`https://picsum.photos/600/350?v=${index}`}
-                  alt="Your alt text"
+                  className="embla__slide__img bg-cover bg-no-repeat bg-center object-cover"
+                  src={slides.image}
+                  alt={slides.title}
                 />
               </div>
             </div>
@@ -103,12 +102,14 @@ const EmblaCarouselTumdnail = (props) => {
           ref={emblaThumbsRef}
         >
           <div className="embla-thumbs__container flex flex-row gap-2 ">
-            {slides.map((index) => (
+            {sliderData.map((slides, index) => (
               <Thumb
-                key={index}
+                key={slides.id}
                 onClick={() => onThumbClick(index)}
                 selected={index === selectedIndex}
                 index={index}
+                image={slides.image}
+                title={slides.title}
               />
             ))}
           </div>
