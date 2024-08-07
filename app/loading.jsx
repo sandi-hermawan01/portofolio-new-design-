8db6d7ca3a36681a2325d1c4e5196787cae648f3
sandi-hermawan01 <sandihermawanf01@gmail.com>
2024-08-07 15:56:24 +0700

    add loading UI

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex h-[80vh] w-full items-center justify-center">
+      <p>Loading....</p>
+    </div>
+  );
+};
+
+export default loading;
